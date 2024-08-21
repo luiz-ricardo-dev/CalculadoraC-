10bcf30810dde64c48134642090b121836d546cb
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-08-21 14:41:49 -0400

    Calculadora simples C#

---
+++ Calculadora/Program.cs
@@ -0,0 +1,10 @@
+﻿using Calculadora.Models;
+
+//Instanciando a classe
+Calculo calc = new Calculo();  
+
+//Chamando os metodos
+calc.Somar(10, 20);
+calc.Subtrair(10, 20);
+calc.Multiplicar(10, 20);
+calc.Dividir(10, 20);
