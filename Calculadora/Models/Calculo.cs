38d14536ece10c3019148dfae2534c40552c69ef
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-08-22 12:11:13 -0400

    Adicionando Funções Trigonométricas

--- Calculadora/Models/Calculo.cs
+++ Calculadora/Models/Calculo.cs
@@ -33,5 +33,26 @@ namespace Calculadora.Models
             double potencia = Math.Pow(x, y);
             Console.WriteLine($"{x}^{y} = {potencia}");
         }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} é = {Math.Round(seno, 4)}");
+        }
+
+         public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} é = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} é = {Math.Round(tangente, 4)}");
+        }
     }
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,9 +4,13 @@
 Calculo calc = new Calculo();  
 
 //Chamando os metodos
-    calc.Somar(10, 20);
-    calc.Subtrair(10, 20);
-    calc.Multiplicar(10, 20);
-    calc.Dividir(10, 20);
+    // calc.Somar(10, 20);
+    // calc.Subtrair(10, 20);
+    // calc.Multiplicar(10, 20);
+    // calc.Dividir(10, 20);
 
-    calc.Potencia(3,3);
+    // calc.Potencia(3,3);
+
+    calc.Seno(30);
+    calc.Coseno(30);
+    calc.Tangente(30);
