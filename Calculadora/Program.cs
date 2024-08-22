4f9013f37deeaf21bb3567dcf433bbd94746e3e0
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-08-22 11:55:55 -0400

    Adicionando o metodo potencia

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,9 @@
 Calculo calc = new Calculo();  
 
 //Chamando os metodos
-calc.Somar(10, 20);
-calc.Subtrair(10, 20);
-calc.Multiplicar(10, 20);
-calc.Dividir(10, 20);
+    calc.Somar(10, 20);
+    calc.Subtrair(10, 20);
+    calc.Multiplicar(10, 20);
+    calc.Dividir(10, 20);
+
+    calc.Potencia(3,3);
--- Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10bcf30810dde64c48134642090b121836d546cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
